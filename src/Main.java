3da279d42b467d6305d131b2d1434c41e443ca71
Alina Prish <alinaprish92@mail.ru>
2022-11-09 15:09:07 +0300

    add HW3.2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int balans = 10;
+        int plus = 3000;
+        int bonus;
+
+        if (plus > 1000) {bonus = plus / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+
+    }
+}
